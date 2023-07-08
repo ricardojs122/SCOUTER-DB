7d05551c4771270aad184821b6a15658183ea9f1
Ricardo Melo <127363426+ricardojs122@users.noreply.github.com>
2023-07-07 21:15:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const scanBtn = document.getElementById('scanBtn');
 
 scanBtn.addEventListener('click', () => {
   // Gerar um valor aleatório entre 1 e 25000 para simular a leitura do poder
-  const powerLevel = Math.floor(Math.random() * 25000) + 1;
+  const powerLevel = Math.floor(Math.random() * 1000000) + 1;
   
   display.innerText = powerLevel;
 });
